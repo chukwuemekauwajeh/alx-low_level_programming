aef33588acd4ac8b32778da9219c1f681675ddbc
chukwuemekauwajeh <uwajehchukwuemeka@gmail.com>
2023-08-15 02:07:10 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
 /**
  * main - entry point
+ *
  * return: 0 success
  */
+int main()
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
