59e271f053bc7507084d46e29318b6cd209320ee
chukwuemekauwajeh <uwajehchukwuemeka@gmail.com>
2023-08-15 01:41:02 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void)
+/**
+ * main - entry point
+ * return: 0 success
+ */
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ *
+ * main - entry point
+ * return: 0 success
+ */	
 {
 	int a;
 	long int b;
